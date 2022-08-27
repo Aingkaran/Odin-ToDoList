4c56d372358607b80e1d41385ff04905b9c3c2d8
Aingkaran <aingkaran1995@gmail.com>
2022-08-26 21:55:07 -0400

    setup webpack and files

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+import './style.css';
+import todoitem from './todo.js';
+
+
+
+
+
+const jamie= todoitem("name", "this is something crazy", "3/4/2022", "HIGH" )
+
+
+console.log(jamie.gettitle())
---
+++ src/todo.js
@@ -0,0 +1,21 @@
+
+
+
+
+const todoitem=(title,description,dueDate,priority)=>{
+
+    let gettitle=()=> title;
+
+    let getdescription=()=>  description;
+
+    let getdueDate=()=> dueDate;
+
+    let getpriority=()=> priority;
+
+    return {gettitle,getdescription,getdueDate,getpriority}
+
+
+
+}
+
+export default todoitem
